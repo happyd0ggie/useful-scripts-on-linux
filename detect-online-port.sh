9ee73a266e3f3b60a64be85c799141182eea6024
sindyoung <deyoung_shen@msn.cn>
2016-05-11 15:05:03 +0800

    add portability

--- detect-online-port.sh
+++ detect-online-port.sh
@@ -3,37 +3,46 @@
 # get ethN
 get_ethn()
 {
-	ifconfig -a | grep -iE "eth.*" | awk -F " " '{ print $1, $5 }' > ethn.list
+	ip link |\
+		grep -Eio '[0-9]+: [a-z0-9]+' |\
+	       	grep -iv lo |\
+	       	awk -F " " '{ print $2 }' > ethn
 }
 
-# get linked status
-get_linked()
+# get mac
+get_conf()
 {
-	eths='./ethn.list'
-	echo -e "PORT\tLINKED\tMAC\t\t\tIP"
-	echo "====================================================="
+	ethns='ethn'
+	echo 'PORT\tLINKED\tMAC\t\t\tIP' 
+	echo "===================================================="
 	while read line
 	do
-		eth=(${line})
-		port=${eth[0]}
-		mac=${eth[1]}
-		# get if linked
-		res=`ethtool ${port} | grep -i detected | awk -F " " '{ print $3 }'`
+		# get mac
+		mac=`ifconfig ${line} |\
+		       	grep -Eio '([a-z0-9]{2}:){5}[a-z0-9]{2}'`
+
 		# get ip
-		ip=`ifconfig ${port} | grep -i addr: | awk -F " " '{ print $2 }' | awk -F ":" '{ print $2 }'`
-		# echo ${port}
-		# echo ${mac}
-		# echo ${ip}
-		echo -n ${port}
-		echo -n -e "\t"
-		echo -n ${res}
-		echo -n -e "\t"
+		ip=`ifconfig ${line} |\
+		grep -Eio '([0-9]{1,3}\.){3}[0-9]{1,3}' |\
+		grep -iv 255`
+
+		# get if linked
+		linked=`ethtool ${line} |\
+		    grep -i detected |\
+		    awk -F " " '{ print $3 }'`
+		
+		echo -n ${line}
+	        echo -n '\t'
+		echo -n ${linked}
+	        echo -n '\t'
 		echo -n ${mac}
-		echo -n -e "\t"
+	        echo -n '\t'
 		echo ${ip}
-	done < ${eths}
-	echo "====================================================="
+	done < ${ethns}
+	echo "===================================================="
 }
 
+clear
 get_ethn
-get_linked
+get_conf
+rm -f ethn
