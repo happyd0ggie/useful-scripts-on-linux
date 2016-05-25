9ae0250775b85c0fd105d1831dc567909f5a4103
sindyoung <deyoung_shen@msn.cn>
2016-05-25 10:44:39 +0800

    udpate detect-online-port.sh

--- detect-online-port.sh
+++ detect-online-port.sh
@@ -3,7 +3,6 @@
 # get ethN
 get_ethn()
 {
-<<<<<<< HEAD
 	ip link |\
 		grep -Eio '[0-9]+: [a-z0-9]+' |\
 	       	grep -iv lo |\
@@ -47,37 +46,3 @@ clear
 get_ethn
 get_conf
 rm -f ethn
-=======
-	ifconfig -a | grep -iE "eth.*" | awk -F " " '{ print $1, $5 }' > ethn.list
-}
-
-# get linked status
-get_linked()
-{
-	eths='./ethn.list'
-	echo -e "PORT\tLINKED\tMAC\t\t\tIP"
-	echo "====================================================="
-	while read line
-	do
-		eth=(${line})
-		port=${eth[0]}
-		mac=${eth[1]}
-		res=`ethtool ${port} | grep -i detected | awk -F " " '{ print $3 }'`
-		ip=`ifconfig ${port} | grep -i addr: | awk -F " " '{ print $2 }' | awk -F ":" '{ print $2 }'`
-		# echo ${port}
-		# echo ${mac}
-		# echo ${ip}
-		echo -n ${port}
-		echo -n -e "\t"
-		echo -n ${res}
-		echo -n -e "\t"
-		echo -n ${mac}
-		echo -n -e "\t"
-		echo ${ip}
-	done < ${eths}
-	echo "====================================================="
-}
-
-get_ethn
-get_linked
->>>>>>> 3be60d991f13e275d691603fc0dd4b12aa7c7403
