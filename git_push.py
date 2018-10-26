fa2c759ec41b1e4bcf5105ad0a2c81900e48a44d
Dexiang Sheng <shengdexiang@gmail.com>
2018-10-26 23:11:33 +0800

    replace tab with 4 spaces

--- git_push.py
+++ git_push.py
@@ -6,11 +6,11 @@ import os
 import sys
 
 def usage():
-	"""
-	1. export github_password='your_github_password'
-	2. ./git_push.py
-	"""
-	print('''
+    """
+    1. export github_password='your_github_password'
+    2. ./git_push.py
+    """
+    print('''
 Usage:
     ./git_push.py
 Note:
@@ -28,28 +28,28 @@ def do_push():
     Raises:
         None.
     """
-	password = os.getenv('github_password')
-	if not password:
-		print('your github password is not set yet')
-		usage()
-		sys.exit(1)
-	cmd = 'git push'
-	git = pexpect.spawn(cmd)
-	while True:
-		index = git.expect(['Username', pexpect.TIMEOUT])
-		if index == 0:
-			break
-		elif index == 1:
-			pass
-		else:
-			pass
-	git.sendline('shengdexiang')
-	git.expect('Password.*')
-	git.sendline(password)
-	git.interact()
+    password = os.getenv('github_password')
+    if not password:
+        print('your github password is not set yet')
+        usage()
+        sys.exit(1)
+    cmd = 'git push'
+    git = pexpect.spawn(cmd)
+    while True:
+        index = git.expect(['Username', pexpect.TIMEOUT])
+        if index == 0:
+            break
+        elif index == 1:
+            pass
+        else:
+            pass
+    git.sendline('shengdexiang')
+    git.expect('Password.*')
+    git.sendline(password)
+    git.interact()
 
 def main():
-	do_push()
+    do_push()
 
 if __name__ == '__main__':
-	main()
+    main()
